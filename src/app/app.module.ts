ad25d6f9087c934ea8cd1a4d3b005a9f086e3c92
Narek Makaryan <nmakaryan1@gmail.com>
2023-08-09 18:10:01 +0400

    overall structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SideBarComponent } from './dashboard/side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule
